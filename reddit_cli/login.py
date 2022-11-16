48aff081e8b7dc0c47c79088c3f0e7de54572f91
Rajat Malviya <rajat@RajatM1.local>
2022-11-16 17:44:38 +0530

    Minor fixes #1

--- reddit_cli/login.py
+++ reddit_cli/login.py
@@ -7,17 +7,23 @@ import requests
 AUTH_REQUEST_URL = "https://www.reddit.com/api/v1/access_token"
 
 
-def get_config_json() -> Dict:
+def get_creds_config() -> Dict:
+    """
+    Get the locally stored credentials expected under ${HOME}/reddit_config.json
+    Returns JSON file as a dictionary
+    """
+    config_dict = None
+
     home_dir = getenv("HOME")
     config_file = f"{home_dir}/reddit_config.json"
 
     # Check and open the config file
-    if path.isfile(config_file):
+    if path.exists(config_file):
         with open(config_file, "r") as f:
             json_contents = f.read()
+        config_dict = json.loads(json_contents)
 
-        print(json_contents)
-    return json.loads(json_contents)
+    return config_dict
 
 
 # TODO: Test this
--- tests/tests.py
+++ tests/tests.py
@@ -1,20 +1,31 @@
 import unittest
-from reddit_cli.login import get_config_json, get_auth_access_token
+from reddit_cli.login import get_creds_config, get_auth_access_token
 import json
 import os
 
 
 class Tests(unittest.TestCase):
-    def test_get_config_json(self):
+    def test_get_creds_config(self):
         val_dict = {"client_id": "abcd", "client_secret": "abcd"}
         home_dir = os.getenv("HOME")
 
         with open(f"{home_dir}/reddit_config.json", "w") as f:
             json.dump(val_dict, f)
 
-        test_dict = get_config_json()
+        test_dict = get_creds_config()
 
         self.assertDictEqual(test_dict, val_dict)
+
+    def test_get_creds_config_empty(self):
+        # Test to see if None is returned if local credentials config is not found
+
+        home_dir = os.getenv("HOME")
+        filename = f"{home_dir}/reddit_config.json"
+        os.remove(filename)
+
+        test_dict = get_creds_config()
+
+        self.assertEqual(test_dict, None)
 
     # TODO
     def test_get_auth_access_token(self):
